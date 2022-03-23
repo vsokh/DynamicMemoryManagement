6c6864d0742ae8218c4b3497c91e02275095ff41
vsokh <vsokolohorskyi@gmail.com>
2022-03-23 16:58:12 +0200

    Support move-semantics, custom deleter

--- main.cpp
+++ main.cpp
@@ -1,38 +1,33 @@
 #include <iostream>
 
-template<typename T>
+template<typename T, typename Deleter = std::function<void(T*)>>
 class SharedPointer
 {
 public:
-    SharedPointer(T* obj)
-        : _cb{new ControlBlock{obj, new size_t{1}}}
-    {
-    }
+    SharedPointer(T* obj) : SharedPointer(obj, [](T* obj){ delete obj; })
+    {}
 
-    ~SharedPointer() {
-        if (_cb->_counter == 0) {
-            delete _cb->_obj;
-            delete _cb;
-        } else {
-            --*_cb->_counter;
-        }
-    }
+    SharedPointer(T* obj, Deleter deleter) : _cb{new ControlBlock{obj, std::move(deleter), 1}}
+    {}
 
     SharedPointer(const SharedPointer<T>& rhs)
-        : _cb{rhs._cb}
-    {
-        // TODO: hangle nullptr cb
-        *rhs._cb->_counter += 1;
-    }
+    { *this = rhs; }
+
+    SharedPointer(SharedPointer<T>&& rhs)
+    { *this = std::move(rhs); }
+
+    ~SharedPointer()
+    { clear(); }
 
     T* operator->() const
     {
-        if (_cb == nullptr)
-        {
+        // TODO: remove
+        if (_cb == nullptr) {
             std::cout << "obj is nullptr" << std::endl;
             return nullptr;
         }
-        return _cb->_obj;
+
+        return _cb ? _cb->_obj : nullptr;
     }
 
     T& operator*() const
@@ -41,18 +36,55 @@ public:
         return *_cb->_obj;
     }
 
+    SharedPointer<T>& operator=(const SharedPointer<T>& rhs)
+    {
+        if (this != &rhs) {
+            clear();
+
+            _cb = rhs._cb;
+            ++_cb->_counter;
+        }
+
+        return *this;
+    }
+
+    SharedPointer<T>& operator=(SharedPointer<T>&& rhs)
+    {
+        if (this != &rhs) {
+            clear();
+
+            _cb = rhs._cb;
+            rhs._cb = nullptr;
+        }
+        return *this;
+    }
+
+    void clear()
+    {
+        if (!_cb) {
+            return;
+        }
+
+        --_cb->_counter;
+        if (_cb->_counter == 0) {
+            _cb->deleter(_cb->_obj);
+            delete _cb; _cb = nullptr;
+        }
+    }
+
     void printFields()
     {
         std::cout << "obj: " << _cb->_obj->i << std::endl;
-        std::cout << "counter: " << *_cb->_counter << std::endl;
+        std::cout << "counter: " << _cb->_counter << std::endl;
     }
 
 private:
     struct ControlBlock
     {
         T* _obj{};
-        size_t* _counter{};
-    } *_cb{};
+        Deleter deleter{};
+        size_t _counter{};
+    }* _cb;
 };
 
 struct S
@@ -60,19 +92,16 @@ struct S
     int i{5};
 };
 
+// For type checking
 template<typename T>
 struct TD;
 
 int main()
 {
-
-    SharedPointer<S> sp1{new S};
-    SharedPointer<S> sp2{sp1};
-    {
-        SharedPointer<S> sp3{sp2};
-        sp3.printFields();
-    }
+    SharedPointer<S> sp1{new S, [](S *s){
+        std::cout << "log: " << s->i << std::endl;
+        delete s;
+    }};
     sp1.printFields();
-    sp2.printFields();
     return 0;
 }
